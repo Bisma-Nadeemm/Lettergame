8ea68b74ed34a85d07c4f817af51420fa3d2c3aa
Bisma-Nadeemm <bissinad@gmail.com>
2023-07-12 05:30:31 -0700

    Initial commit

---
+++ app/src/main/java/com/example/lettergame/DisplayFragment.java
@@ -0,0 +1,14 @@
+package com.example.lettergame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_fragment);
+    }
+}
---
+++ app/src/main/java/com/example/lettergame/TestResultAdapter.java
@@ -0,0 +1,4 @@
+package com.example.lettergame;
+
+public class TestResultAdapter {
+}
